b8a629ba84a198e2a553a13ae7952cacf42494be
jessiropa <kristinjessi9700@gmail.com>
2025-10-03 22:14:21 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+    <h1>hello react</h1>
+    <p>Aku lagi belajar react pertama kali</p>
+   </div>
+  );
+}
+
+export default App
