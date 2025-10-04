1df406c4e4ee55010def97409186e3b1cd9a098f
jessiropa <kristinjessi9700@gmail.com>
2025-10-04 17:24:05 +0700

    belajar mengenai components

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+
+function Welcome(){
+  return <h1>Hallo, ini adalah component Welcome!</h1>
+}
 
 function App() {
 
   const name = "Jessi Kristin";
 
   return (
-   <div>
-    <h1>hello, {name}!</h1>
-    <p>Aku lagi belajar react pertama kali</p>
-   </div>
+  //  <div>
+  //   <h1>hello, {name}!</h1>
+  //   <p>Aku lagi belajar react pertama kali</p>
+  //  </div>
+  <div>
+    <Welcome/>
+    <Welcome/>
+    <Welcome/>
+    <Welcome/>
+  </div>
   );
 }
 
